c9086e0327997589402b06e639c3d11f4430fb36
Sesugh Asa <74496255+elsesu@users.noreply.github.com>
2024-07-06 10:35:40 +0100

    push again

--- trips-planner/src/main.js
+++ trips-planner/src/main.js
@@ -3,12 +3,11 @@ import  {store}  from './components/store'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { createPinia } from 'pinia'
 import './index.css'
 
 
 const app = createApp(App)
-const pinia = createPinia()
+
 
 app.use(router)
 
